71787cf7e16b3624ff01a9c372c8b76af3e86160
louischausse-auxilio <85704673+louischausse-auxilio@users.noreply.github.com>
2023-11-07 16:02:58 -0500

    Update retry.js
    make code object agnostic

--- retry.js
+++ retry.js
@@ -4,16 +4,16 @@ exports.main = async (event, callback) => {
   
   //define variables
   const opsToken = process.env.SERIAL_RECORD_ID_GENERATOR
-  const objectType = "2-13510438"; // this the type id of the custom object project used in this example. Could be replaced with any standard object (contacts, deals, companies, tickets) or another custom object type id 2-XXXXX
-  const properties = ["serial_number_id"];
-  const latestProjectId = event.inputFields['latestProjectId'];
-  const objectId = latestProjectId
+  const objectType = "2-13510438"; // This is the type id of the custom object used in this example. Could be replaced with any standard object (contacts, deals, companies, tickets) or another custom object type id 2-XXXXX
+  const properties = ["serial_number_id"]; // This is the name of your serial number property. This property needs to be created on the object first. 
+  const latestRecordId = event.inputFields['latestRecordId'];
+  const objectId = latestRecordId
 
   //define reusable function
   
   const hubspotClient = new hubspot.Client({"accessToken":opsToken});
   
-  async function getProject() {
+  async function getRecord() {
     try {
       const apiResponse = await hubspotClient.crm.objects.basicApi.getById(objectType, objectId, properties);
       return apiResponse
@@ -22,28 +22,28 @@ exports.main = async (event, callback) => {
     }
   }
 
-  // get the latest project
+  // get the latest record
 
-  getProject()
-    .then(latestProject => {
-	const latestProjectNumber = latestProject.properties.serial_number_id
+  getRecord()
+    .then(latestRecord => {
+	const latestRecordNumber = latestRecord.properties.serial_number_id
     
-    if (latestProjectNumber) {
-      console.log(`Latest project number = ${latestProjectNumber} and ID = ` + latestProjectId)
+    if (latestRecordNumber) {
+      console.log(`Latest number = ${latestRecordNumber} and ID = ` + latestRecordId)
 
-      const projectNumber = parseInt(latestProjectNumber) + 1;
-      console.log(`New project number = ` + projectNumber);
+      const recordNumber = parseInt(latestRecordNumber) + 1;
+      console.log(`New number = ` + recordNumber);
 
       callback({
         outputFields: {
-          projectNumber: projectNumber,
-          nextAction: "setProjectNumber"
+          recordNumber: recordNumber,
+          nextAction: "setRecordNumber"
         }
       });
     } else {
       callback({
         outputFields: {
-          projectNumber: "NaN",
+          recordNumber: "NaN",
           nextAction: "retry"
         }
       });
